94bd05d19350a3141ec8dff3e67a8fdd78eb55dc
DongJun Kim <79851833+Dev-Jun125@users.noreply.github.com>
2021-03-23 01:46:10 +0900

    Add files via upload

---
+++ 2주차과제.cpp
@@ -0,0 +1,141 @@
+//48p. 2번
+/*
+#include <iostream>
+
+using namespace std;
+int main(void)
+{
+	cout << "*********************************\n";
+	cout << "	* 리포트 *\n";
+	cout << "*********************************\n";
+	cout << "\t-----------------------\n";
+	cout << "\t|항공소프트웨어공학과|\n";
+	cout << "\t-----------------------\n";
+	cout << "\t201600882\n";
+	cout << "\t김동준\n";
+};
+*/
+//48p.3번
+/*
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	double a, b;
+	cout << "실수를 입력하세요. :";
+	cin >> a;
+	b = a * 1.5;
+	cout << "입력한 갑의 1.5배는" << b << "입니다.";
+}
+*/
+//49p. 4번
+/*
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int a, b, area;
+	cout << "가로의 길이를 입력하세요.:";
+	cin >> a;
+	cout << "세로의 길이를 입력하세요.:";
+	cin >> b;
+	area = a * b;
+	cout << "사각형의 면적은" << area << "입니다.";
+}
+*/
+//51p. 1번
+/*
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	cout << "+++++++++\n";
+	cout << "대한민국\n";
+	cout << "+++++++++\n";
+}
+*/
+//52p. 5번
+/*
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	cout << "* * * * *\n";
+	cout << " * * * *\n";
+	cout << "  * * *\n";
+	cout << "   * *\n";
+	cout << "    *";
+}
+*/
+//52p. 6번
+/*
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int a;
+	cout << "당신의 나이를 입력하세요. :";
+	cin >> a;
+	cout << "당신이 살아온 날은 " << a * 365 << "일 입니다.";
+}
+*/
+//53p. 7번
+/*
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(void)
+{
+	double r;
+	cout << "원의 반지름을 입력하세요. :";
+	cin >> r;
+	cout << "원의 부피는 " << 0.75 * pow(r, 3) << " 입니다.";
+}
+*/
+//53p. 8번
+/*
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int a, b, c;
+	cout << "a값을 입력하세요. : ";
+	cin >> a;
+	cout << "b값을 입력하세요. : ";
+	cin >> b;
+	cout << "c값을 입력하세요. : ";
+	cin >> c;
+	cout << "모든 값의 합은 " << a + b + c << "입니다.\n";
+	cout << "모든 값의 평균은 " << (a + b + c) / 3.0 << "입니다.\n";
+}
+*/
+//53p. 9번
+/*
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(void)
+{
+	int a;
+	cout << "정수값을 입력하시오. :";
+	cin >> a;
+	cout << "입력받은 정수의 값은 " << a << "입니다.\n";
+	cout << "입력받은 정수의 값  의 제곱값은 " << pow(a, 2) << "입니다.\n";
+	cout << "입력받은 정수의 값의 세제곱값은 " << pow(a, 3) << "입니다.\n";
+}
+*/
+//54p.10번
+/*
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int f;
+	double c;
+	cout << "화씨온도를 입력하세요. :";
+	cin >> f;
+	c = (5.0 / 9.0 * f) - 32;
+	cout << "섭씨 온도는 " << c << "입니다.";
+
+}
+*/
