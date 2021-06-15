4db0942dd28f468c3aa83ae26b8788895e923f9a
DongJun Kim <dev.jun125@gmail.com>
2021-06-15 23:46:08 +0900

    c++ 프로그래밍 수업

--- 14주차수업.cpp
+++ 14주차수업.cpp
@@ -401,6 +401,7 @@ int main()
 	delete pShape;
 }
 */
+/*
 #include <iostream>
 using namespace std;
 
@@ -419,3 +420,40 @@ public:
 	}
 
 };
+*/
+#include <iostream>
+using namespace std;
+class MyCar {
+private:
+	//멤버변수
+	int fuel;
+	bool power;
+
+public:
+	//메소드
+	void go() {
+		this->fuel--;
+	}
+
+	void oiling(int n) {
+		this->fuel += n;
+	}
+
+	void fuel_check() {
+		cout << "연료 : " << fuel << endl;
+	}
+};
+
+int main()
+{
+	MyCar car = MyCar(); //생성
+
+//메서드 호출
+	car.oiling(100);
+	car.fuel_check();
+	for (int i = 0; i < 10; i++) car.go();
+	car.fuel_check();
+	car.oiling(100);
+	for (int i = 0; i < 10; i++) car.go();
+	car.fuel_check();
+}
